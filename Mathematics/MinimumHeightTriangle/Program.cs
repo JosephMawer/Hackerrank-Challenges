411f2b4dcfff17e02226c3bc581400796367bb56
Joseph Mawer <joseph.mawer@caseware.com>
2019-11-14 21:09:31 -0500

    Added minimum triangle height project

---
+++ Mathematics/MinimumHeightTriangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MinimumHeightTriangle
+{
+    // https://www.hackerrank.com/challenges/lowest-triangle/problem
+
+    class Program
+    {
+        static int lowestTriangle(int tbase, int area)
+            => (int)Math.Ceiling((area * 2) / (decimal)tbase);
+
+        static void Main(string[] args)
+        {
+            string[] tokens_base = Console.ReadLine().Split(' ');
+            var tbase = Convert.ToInt32(tokens_base[0]);
+            var area = Convert.ToInt32(tokens_base[1]);
+            var height = lowestTriangle(tbase, area);
+            Console.WriteLine(height);
+        }
+    }
+}
