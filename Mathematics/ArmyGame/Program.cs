018503a57ace66fcd437395e7c5e307869b1711f
Joseph Mawer <joseph.mawer@caseware.com>
2019-11-16 13:18:54 -0500

    comments

--- Mathematics/ArmyGame/Program.cs
+++ Mathematics/ArmyGame/Program.cs
@@ -14,15 +14,16 @@ namespace ArmyGame
             Console.WriteLine(gameWithCells(70, 123));
         }
 
-        // calculate the number of drop off points for a n * m grid, given the fact that a single drop off
-        // can cover at most 4 cells (if arranged in a 2*2 grid), otherwise at most 2 cells if arranged horizontally or vertically
+        // calculate the number of drop off points for a n * m grid, given the fact that
+        // a single drop off can cover at most 4 cells (if arranged in a 2*2 grid),
+        // otherwise at most 2 cells if arranged horizontally or vertically
         static int gameWithCells(int n, int m)
         {
             var area = n * m;
             int rows = n, columns = m;
 
             // base case 1. handle single columns/rows first
-            if (rows == 1 || columns == 1) { // assume we have a single row or column means at most we can only cover 2 cells
+            if (rows == 1 || columns == 1) { 
                 return (int)Math.Ceiling(n * m / 2M);
             }
 
@@ -30,11 +31,11 @@ namespace ArmyGame
             if (area <= 2 || area == 4) return 1;
 
             int extraRow = 0,extraColumn = 0;
-            if (rows % 2 != 0) { // we need to calculate drop offs for an additional row
+            if (rows % 2 != 0) { // drop offs for an additional row
                 extraRow = (int) Math.Ceiling(columns / 2M);
             }
 
-            if (columns % 2 != 0) { // we need to calculate drop offs for an additional column
+            if (columns % 2 != 0) { // drop offs for an additional column
                 extraColumn = (int)Math.Ceiling(rows / 2M);
             }
 
